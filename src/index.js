0bc5a8975b90ddcd1961412bebbf9724f3d42194
ellentrj <125470257+ellentrj@users.noreply.github.com>
2023-03-17 10:29:32 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//forma literal
+let produto = {
+            nome:'celular',
+            preco:5000,
+            desconto:0,9
+}
+produto.precofinal= produto.preco * produto.desconto
+console.log(produto)
+
+//forma dinamica
+let pessoa={}
+pessoa.nome = "cleiton"
+pessoa.idade = 22
+pessoa.altura = 1.98
+
+console.log(pessoa)
